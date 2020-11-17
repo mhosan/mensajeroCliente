d719459e6cc3e24d7612e261a242e73fdc5ceb3e
marcelo <mhosan@gmail.com>
2020-11-17 18:14:28 -0300

    agregando delete de clave cambiada

--- main.js
+++ main.js
@@ -14,21 +14,27 @@ function urlBase64ToUint8Array(base64String) {
     }
     return outputArray;
 }
-//https://mensajeropush.herokuapp.com/subscription
-//http://localhost:3000/subscription'
 
+const urlLocal = 'http://localhost:3000/subscription';
+const urlRemota = 'https://mensajeropush.herokuapp.com/subscription';
+
+//---------------------------------------------------------------------
+// mh: 17/11/20
+// Registrar el service worker si este se encuentra disponible en el 
+// navegador del usuario.
+// Si la registración del web worker es exitosa, se envia una subscripción
+// al servidor en una petición POST y se almacena en localStorage la clave
+// auth generada.
+//---------------------------------------------------------------------
 const subscription = async () => {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('./worker.js', { scope: '/' })
             .then(function (registration) {
                 console.log('Service worker registration succeeded:', registration);
-
                 const register = registration;
-                
                 register.pushManager.subscribe({ userVisibleOnly: true, applicationServerKey: urlBase64ToUint8Array(PUBLIC_VAPID_KEY) })
                     .then((subscription) => {  //subscription es el objeto que va a utilizar el servidor para comunicarse
-
-                        fetch('http://localhost:3000/subscription', {
+                        fetch(urlLocal, {
                             method: 'POST',
                             mode: 'cors',
                             body: JSON.stringify(subscription),
@@ -37,7 +43,12 @@ const subscription = async () => {
                             }
                         })
                             .then(() => {
-                                console.log('suscripto ok!');
+                                const laSuscrip = JSON.stringify(subscription);
+                                const quebrado = laSuscrip.split("auth");
+                                const elAuth = quebrado[1].substring(1);
+                                //guardar la subscripción en localStorage
+                                localStorage.setItem('auth', elAuth);
+                                console.log(`Suscripto ok!. El auth: ${elAuth} se guardó localmente.`);
                             })
                             .catch(err => {
                                 //alert(err)
@@ -50,14 +61,40 @@ const subscription = async () => {
     } else {
         console.log('Service workers are not supported.');
     }
-
 }
 subscription();
 
+//---------------------------------------------------------------------
+// mh: 17/11/20
+// Detectar cuando el usuario hace cambios en los permisos de recibir
+// notificaciones. SOLO en el caso de cambiar el permiso a denied se 
+// enviará al servidor un pedido de borrar la clave (auth) que estaba 
+// utilizando el usuario hasta ese momento.
+// Si el usuario vuelve a permitir las notificaciones push, se generará
+// una nueva clave.
+//---------------------------------------------------------------------
 if ('permissions' in navigator) {
     navigator.permissions.query({ name: 'notifications' }).then(function (notificationPerm) {
         notificationPerm.onchange = function () {
-            console.log("Parece que el usuario cambió los permisos. Nuevo permiso: " + notificationPerm.state);
+            console.log("El usuario cambió los permisos. Nuevo permiso: " + notificationPerm.state);
+            if (notificationPerm.state == 'denied') {
+                console.log(`Se enviará una petición de borrar la clave ${localStorage.getItem('auth')}`)
+                // fetch(urlLocal, {
+                //                 method: 'DELETE',
+                //                 mode: 'cors',
+                //                 body: localStorage.getItem('auth'),
+                //                 headers: {
+                //                     'Content-Type': 'application/json'
+                //                 }
+                //             })
+                //                 .then(() => {
+                //                     console.log('Se envió el pedido de borrar la subscripción ok!');
+                //                 })
+                //                 .catch(err => {
+                //                     //alert(err)
+                //                     console.log('error!', err);
+                //                 })
+            }
         };
     });
 }
