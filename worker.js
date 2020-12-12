c9cc250ff0df766cc7dd991fdca298e23a37e3ab
marcelo <mhosan@gmail.com>
2020-12-12 17:31:50 -0300

    probando opciones de las notif

--- worker.js
+++ worker.js
@@ -12,32 +12,3 @@ self.addEventListener('push', event => {
     event.waitUntil(promiseChain);
 });
 
- // .then(() => {
-    //     console.log(`Notificación ok!, data: ${data}`);
-    // })
-    // .catch(err => {
-    //     console.log(`Error en el worker en showNotification: ${err}`)
-    // })
-    // self.addEventListener('push', e => {
-    //     const data = e.data.json();
-    //     console.log(data);
-    //const promiseChain = self.registration.showNotification('Hello, World.');
-    //e.waitUntil(promiseChain);
-    // self.registration.showNotification(
-    //     data.title,
-    //     {
-    //         body: data.message,
-    //         icon: '/images/marcador.png'
-    //     });
-    // self.registration.showNotification(
-    //          data.title,
-    //     {
-    //         body: data.message,
-    //         icon: '/images/marcador.png'
-    //     })
-    //     .then((resultado)=>{
-    //         console.log(`Notificación ok!`);
-    //     })
-    //     .catch((err)=>{ console.log(`Error en el worker en showNotification: ${err}`)})
-//});
-
